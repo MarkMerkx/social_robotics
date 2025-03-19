84c77266759e60ac00f32a2790ed18b131ee7fd7
MarkMerkx <67427825+MarkMerkx@users.noreply.github.com>
2025-03-19 13:33:36 +0100

    Update dialogue_with_reprompt.py

--- assignment_3/game_control/reprompt/dialogue_with_reprompt.py
+++ assignment_3/game_control/reprompt/dialogue_with_reprompt.py
@@ -3,13 +3,10 @@ def dialogue_with_reprompt(prompt, gesture, n, session, stt, timeout=20):
     yield sleep(5)
 
     # Wait for the user's answer.
-    reponse = yield wait_for_response(None, session, stt, timeout=20)
+    reponse = yield wait_for_response(None, session, stt, timeout=timeout)
     if not reponse:
         reponse = "No response"
         logger.debug("No feedback received; defaulting to: %s", feedback)
-    else:
-        logger.debug("Feedback received: %s", feedback)
-        return
+        return dialogue_with_reprompt(prompt, gesture, n =+1 , session, stt, timeout)  # call recursively
+    return
 
-    n += 1
-    return dialogue_with_reprompt() # call recursively
